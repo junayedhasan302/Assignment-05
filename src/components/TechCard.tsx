ab0b923e4fc5a5d8ad7321659b1fcea5a33178a7
junayedhasan302 <junayedhasan302@gmail.com>
2026-09-13 23:08:47 +0600

    Red color added, Full respomsive for mobile, tab, pc, and its finel code

--- src/components/TechCard.tsx
+++ src/components/TechCard.tsx
@@ -6,6 +6,7 @@ interface TechCardProps {
   isAdded: boolean;
   onAdd: (tech: TechnologyType) => void;
 }
+
 // Badge Color Conditions
 function getBadgeColor(colorTheme: string) {
   if (colorTheme === "orange") {
@@ -24,6 +25,8 @@ function getBadgeColor(colorTheme: string) {
     return "bg-green-100 text-green-700";
   } else if (colorTheme === "black") {
     return "bg-gray-100 text-gray-700";
+  } else if (colorTheme === "red") {
+    return "bg-red-100 text-red-700";
   } else {
     return "bg-gray-100 text-gray-700";
   }
@@ -47,6 +50,8 @@ function getHoverBackground(colorTheme: string) {
     return "hover:bg-green-50 active:bg-green-50";
   } else if (colorTheme === "black") {
     return "hover:bg-gray-50 active:bg-gray-50";
+  } else if (colorTheme === "red") {
+    return "hover:bg-red-50 active:bg-red-50";
   } else {
     return "hover:bg-gray-50 active:bg-gray-50";
   }
@@ -55,34 +60,53 @@ function getHoverBackground(colorTheme: string) {
 const TechCard = ({ tech, isAdded, onAdd }: TechCardProps) => {
   return (
     // Card Div
-    <div className={`bg-white border border-gray-200 rounded-xl sm:rounded-2xl p-4 sm:p-5 shadow-sm hover:shadow-lg active:shadow-lg hover:-translate-y-[1px] active:-translate-y-[1px] transition duration-200 h-full flex flex-col ${getHoverBackground(tech.colorTheme)}`}>
+    <div
+      className={`bg-white border border-gray-200 rounded-xl sm:rounded-2xl p-4 sm:p-4.5 md:p-5 shadow-sm hover:shadow-lg active:shadow-lg hover:-translate-y-[1px] active:-translate-y-[1px] transition duration-200 h-full flex flex-col ${getHoverBackground(
+        tech.colorTheme
+      )}`}
+    >
       {/* Logo, Badge  */}
-      <div className="flex justify-between items-center mb-3 sm:mb-4">
+      <div className="flex justify-between items-center mb-3 sm:mb-3.5 md:mb-4">
         {/* Logo */}
-        <img src={tech.icon} alt={tech.name} className="w-7 h-7 sm:w-8 sm:h-8 object-contain" />
+        <img
+          src={tech.icon}
+          alt={tech.name}
+          className="w-7 h-7 sm:w-7.5 sm:h-7.5 md:w-8 md:h-8 object-contain"
+        />
         {/* Badge  */}
-        <span className={`text-[10px] sm:text-xs px-2 sm:px-2.5 py-0.5 sm:py-1 rounded-full font-medium ${getBadgeColor(tech.colorTheme)}`}>
+        <span
+          className={`text-[10px] sm:text-[11px] md:text-xs px-2 sm:px-2 md:px-2.5 py-0.5 md:py-1 rounded-full font-medium ${getBadgeColor(
+            tech.colorTheme
+          )}`}
+        >
           {tech.badge}
         </span>
       </div>
 
-      {/* Technology NAme  */}
-      <h3 className="font-bold text-gray-900 text-sm sm:text-base mb-1">{tech.name}</h3>
+      {/* Technology Name  */}
+      <h3 className="font-bold text-gray-900 text-sm sm:text-[15px] md:text-base mb-1">
+        {tech.name}
+      </h3>
       {/* Technology Description */}
-      <p className="text-sm sm:text-base font-medium text-gray-500 mb-3 sm:mb-4 leading-relaxed">
+      <p className="text-xs sm:text-sm md:text-base font-medium text-gray-500 mb-3 sm:mb-3.5 md:mb-4 leading-relaxed">
         {tech.description}
       </p>
 
       {/* category, difficulty, rating  */}
-      <div className="flex flex-wrap items-center gap-1.5 sm:gap-2 text-[10px] sm:text-xs text-gray-500 mb-3 sm:mb-4">
-        <span className="bg-gray-200 px-1.5 sm:px-2 py-0.5 sm:py-1 rounded-md">{tech.category}</span>
-        <span className="bg-gray-200 px-1.5 sm:px-2 py-0.5 sm:py-1 rounded-md">{tech.difficulty}</span>
+      <div className="flex flex-wrap items-center gap-1.5 md:gap-2 text-[10px] sm:text-[11px] md:text-xs text-gray-500 mb-3 sm:mb-3.5 md:mb-4">
+        <span className="bg-gray-200 px-1.5 md:px-2 py-0.5 md:py-1 rounded-md">
+          {tech.category}
+        </span>
+        <span className="bg-gray-200 px-1.5 md:px-2 py-0.5 md:py-1 rounded-md">
+          {tech.difficulty}
+        </span>
         <span className="flex items-center gap-1">⭐ {tech.rating}</span>
       </div>
 
-      {/* Add to cart Button */}
-      <button onClick={() => onAdd(tech)}
-      disabled={isAdded}
+      {/* Add to Stack Button */}
+      <button
+        onClick={() => onAdd(tech)}
+        disabled={isAdded}
         className={
           isAdded
             ? "w-full bg-gray-200 text-gray-500 rounded-lg py-2 sm:py-2.5 text-xs sm:text-sm font-medium mt-auto"
